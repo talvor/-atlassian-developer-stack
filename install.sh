341c5e5123facce334b57ac9da22524e486f9a2c
diegoraferreira <dferreira@atlassian.com>
2019-12-17 10:19:59 +1100

    Formating the install.sh file

--- install.sh
+++ install.sh
@@ -7,28 +7,24 @@ MKDIR="$(which mkdir) -p"
 GIT="$(which git)"
 ASD_REPOSITORY="git@bitbucket.org:phall_atlassian/atlassian-developer-stack.git"
 
-install()
-{
+install() {
     checkDependencies
     setUpAsdDirectory
     cloneRepository
 }
 
-log()
-{
-    if  "$DEBUG" = true ; then
+log() {
+    if "$DEBUG" = true; then
         echo $(date "+%d-%m-%Y - %T") : $1
     fi
 }
 
 # Check all dependencies needed to run ASD, it will abort the execution if any of the dependencies is missing.
-checkDependencies()
-{
+checkDependencies() {
     log "Checking all dependencies needed before we start ... "
 
     dependencies=("docker" "git")
-    for dependency in "${dependencies[@]}"
-    do 
+    for dependency in "${dependencies[@]}"; do
         if [ "$(which $dependency)" = "" ]; then
             log "Atlassian Developer Stack requires '$dependency' to be installed in your enviroment. Please, fix it and try again"
             exit 1
@@ -39,12 +35,11 @@ checkDependencies()
 }
 
 # Set up the ASD directory, it will create the directory ($ASD_DIRECTORY_PATH) for current user if it does not exits.
-setUpAsdDirectory()
-{
-    log  "Creating the asd's configuration folder for user '$USER' which is the user who is running this script"
+setUpAsdDirectory() {
+    log "Creating the asd's configuration folder for user '$USER' which is the user who is running this script"
     if [ ! -d $ASD_DIRECTORY_PATH ]; then
         log "The configuration folder '$ASD_DIRECTORY_PATH' does not exist, let's create it"
-        $MKDIR $ASD_DIRECTORY_PATH;
+        $MKDIR $ASD_DIRECTORY_PATH
         log "Folder '$ASD_DIRECTORY_PATH' successfully created"
     else
         log "The configuration folder '$ASD_DIRECTORY_PATH' already exists"
@@ -52,15 +47,14 @@ setUpAsdDirectory()
 }
 
 # Clone the ASD repository
-cloneRepository()
-{
-   if [ `ls -A $ASD_DIRECTORY_PATH | wc -l` -eq 0 ]; then
+cloneRepository() {
+    if [ $(ls -A $ASD_DIRECTORY_PATH | wc -l) -eq 0 ]; then
         log "Cloning the repository"
         $GIT clone $ASD_REPOSITORY $ASD_DIRECTORY_PATH
-   else 
+    else
         log "Updating the repository"
         $GIT -C $ASD_DIRECTORY_PATH pull
-   fi
+    fi
 }
 
 # Running the Installation
