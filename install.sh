cea16b19f4d89fddd1eb06cc2fd72154ea25e448
diegoraferreira <dferreira@atlassian.com>
2019-12-16 17:19:45 +1100

    Setting up the instalation directory, checking dependencies and cloning the repository

--- install.sh
+++ install.sh
@@ -1,21 +1,69 @@
 #!/bin/bash
 
-# Check all dependencies needed to run Atlassian Developer Stack
-function checkDependencies(){
-    echo "Checking all dependencies needed before we start ... "
+#global variables
+DEBUG=true
+ASD_DIRECTORY_PATH="/Users/$USER/.atlassian/asd"
+MKDIR="/bin/mkdir -p"
+GIT="/usr/local/bin/git"
+ASD_REPOSITORY="git@bitbucket.org:phall_atlassian/atlassian-developer-stack.git"
+
+install()
+{
+    checkDependencies
+    setUpAsdDirectory
+    cloneRepository
+}
+
+log()
+{
+    if  "$DEBUG" = true ; then
+        echo $(date "+%d-%m-%Y - %T") : $1
+    fi
+}
+
+# Check all dependencies needed to run ASD, it will abort the execution if any of the dependencies is missing.
+checkDependencies()
+{
+    log "Checking all dependencies needed before we start ... "
 
     dependencies=("docker" "git")
     for dependency in "${dependencies[@]}"
     do 
-        if [ "$(which $dependency)" = "" ] ;then
-            echo "Atlassian Developer Stack requires '$dependency' to be installed in your enviroment. Please, fix it and try again."
+        if [ "$(which $dependency)" = "" ]; then
+            log "Atlassian Developer Stack requires '$dependency' to be installed in your enviroment. Please, fix it and try again"
             exit 1
         fi
     done
 
-    echo "Nice!!! You have all dependencies installed :-) ... Let's move on!"
+    log "Nice!!! You have all dependencies installed :-) ... Let's move on!"
 }
 
-# Running the script
-echo "Install Atlassian Developer Stack (asd)"
-checkDependencies
+# Set up the ASD directory, it will create the directory ($ASD_DIRECTORY_PATH) for current user if it does not exits.
+setUpAsdDirectory()
+{
+    log  "Creating the asd's configuration folder for user '$USER' which is the user who is running this script"
+    if [ ! -d $ASD_DIRECTORY_PATH ]; then
+        log "The configuration folder '$ASD_DIRECTORY_PATH' does not exist, let's create it"
+        $MKDIR $ASD_DIRECTORY_PATH;
+        log "Folder '$ASD_DIRECTORY_PATH' successfully created"
+    else
+        log "The configuration folder '$ASD_DIRECTORY_PATH' already exists"
+    fi
+}
+
+# Clone the ASD repository
+cloneRepository()
+{
+   if [ `ls -A $ASD_DIRECTORY_PATH | wc -l` -eq 0 ]; then
+        log "Clonning the repository"
+        $GIT clone $ASD_REPOSITORY $ASD_DIRECTORY_PATH
+   else 
+        log "Updating the repository"
+        $GIT -C $ASD_DIRECTORY_PATH pull
+   fi
+}
+
+# Running the Installation
+echo "Installing Atlassian Developer Stack (asd)"
+install
+echo "Atlassian Developer Stack (asd) successfully installed"
