4f724678a557ed2babdeb7b2f598850b3d8b853a
diegoraferreira <dferreira@atlassian.com>
2019-12-16 14:11:06 +1100

    Beta version of install script

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+# Check all dependencies needed to run Atlassian Developer Stack
+function checkDependencies(){
+    echo "Checking all dependencies needed before we start ... "
+
+    dependencies=("docker" "git")
+    for dependency in "${dependencies[@]}"
+    do 
+        if [ "$(which $dependency)" = "" ] ;then
+            echo "Atlassian Developer Stack requires '$dependency' to be installed in your enviroment. Please, fix it and try again."
+            exit 1
+        fi
+    done
+
+    echo "Nice!!! You have all dependencies installed :-) ... Let's move on!"
+}
+
+# Running the script
 echo "Install Atlassian Developer Stack (asd)"
+checkDependencies
