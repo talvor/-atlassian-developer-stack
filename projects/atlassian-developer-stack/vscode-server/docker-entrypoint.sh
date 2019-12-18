e58274d0463f4eb9b377882b84b26771e1fa4cc2
Phillip Hall <phall@atlassian.com>
2019-12-18 13:15:15 +1100

    Fix multiple project issues

--- projects/atlassian-developer-stack/vscode-server/docker-entrypoint.sh
+++ projects/atlassian-developer-stack/vscode-server/docker-entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Clone repository
 if [ ! -d ~/workspace/.git ]; then
   git clone $GIT_CLONE_URL ~/workspace
 fi;
+
+# Install vscode extensions
+extensions=("esbenp.prettier-vscode")
+for extension in "${extensions[@]}"; do
+  code-server --install-extension ${extension}
+done
 
 exec "$@"
